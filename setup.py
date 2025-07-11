6ad5959c6da53a932b3a1eb72f05f92c7283a249
amin nabiyev <a.nabiyev@adestis.de>
2025-07-11 15:09:31 +0200

    versions erhöhung

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
     description = f.read()
 setup(
     name='adestis-netbox-certificate-management',
-    version='1.0.1',
+    version='1.0.4',
     description='ADESTIS Certificate Management',
     url = 'https://github.com/an-adestis/ADESTIS-Netbox-Certificate-Management',
     author='ADESTIS GmbH',
